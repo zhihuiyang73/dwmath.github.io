421b3d2885f33618d6cd5630d9fdf58957abbf0d
Zhihui Yang <zhihui.yang73@gmail.com>
2020-12-28 14:56:19 -0600

    intial commit

---
+++ javaScript/load_shared.js
@@ -0,0 +1,7 @@
+
+
+    document.getElementById("headerTitle").innerHTML = "<h2><a href='DW_Math.html'> DW Math Interest Group</a></h2>"
+                                                + "<p>This is the Homepage of the site. Write something here...</p>";
+  
+
+    document.getElementById("myFooter").innerHTML = " <p>any thought what we should do here? ... </p>";
